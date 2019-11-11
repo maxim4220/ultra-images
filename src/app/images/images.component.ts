80a38363cca0d27a49bc794f495169d5af5cd2a7
Maxim Baranov <maxim4220@gmail.com>
2019-11-11 16:07:17 +0200

    Added logic to load images based on pagination

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -3,6 +3,7 @@ import { ImageService } from './services/image.service';
 import { finalize, takeUntil, tap } from 'rxjs/operators';
 import { Subject, Subscription } from 'rxjs';
 import { ImageSearchService } from '../ui/image-search/image-search.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-images',
@@ -12,27 +13,27 @@ import { ImageSearchService } from '../ui/image-search/image-search.service';
 export class ImagesComponent implements OnInit, OnDestroy {
   public images;
   public showSpinner: boolean;
-  private unsubscribe: Subject<any>;
+  public pagination;
 
+  private unsubscribe: Subject<any>;
   private subs: Subscription;
 
-  constructor(private imageService: ImageService, private imageSearchService: ImageSearchService) {
+  constructor(private imageService: ImageService, private imageSearchService: ImageSearchService,
+      private activatedRoute: ActivatedRoute) {
     this.unsubscribe = new Subject();
   }
 
-  // API key: krYLN9xmR99j1WQkm5nERGA0w3bXmD2D
   ngOnInit() {
     this.showSpinner = true;
-    this.subs = this.imageSearchService.searchImages$.subscribe( images => {
-      if(images && images.length > 0) {
-        console.log('IMAGES SUBS IF!!', images);
-        this.images = images;
+    this.subs = this.imageSearchService.searchImages$.subscribe( response => {
+      if(response && response.data.length > 0) {
+        this.images = response.data;
+        this.pagination = response.pagination;
         this.showSpinner = false;
-      } else if(images && images.length === 0) {
-        console.log('NO IMAGES FOUND!');
+      } else if(response && response.data.length === 0) {
+        this.pagination = null;
         this.images = null;
       } else {
-        console.log('else - load default images.');
         this.loadImages();
       }
     })
@@ -42,21 +43,38 @@ export class ImagesComponent implements OnInit, OnDestroy {
     this.subs.unsubscribe();
   }
 
-  // Test method
-  private loadImages() {
-   
-   const searchParams = '10&limit=25&offset=0&rating=G&lang=en';
+  private loadImages(q?, offset?) {
+   let searchParams = '';
+   if(offset) {
+    searchParams += offset
+   }
+   if(q) {
+    searchParams += q;
+   } else {
+    searchParams += '&q=10';
+   }
     this.imageService.getImages(searchParams)
     .pipe(
       tap(response => {
-        console.log('response', response);
         this.images = response.data;
+        this.pagination = response.pagination;
       }),
       takeUntil(this.unsubscribe),
       finalize(() => {
       this.showSpinner = false;
       })
   ).subscribe();
+  }
+
+ public  getPagination(event) {
+    const searchQuery = this.activatedRoute.snapshot.paramMap.get('searchQuery');
+    const offset = '&offset=' + event;
+    if(searchQuery) {
+      const q = '&q=' + searchQuery;
+    this.loadImages(q, offset);
+    } else {
+      this.loadImages(null, offset);
+    }
   }
 
 }
