d41dd4734b4703251f6b3e3aff0c60a394ae384f
Maxim Baranov <maxim4220@gmail.com>
2019-11-11 16:02:41 +0200

    Removed unused code.

--- src/app/ui/image-search/image-search.service.ts
+++ src/app/ui/image-search/image-search.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ImageSearchService {
- public searchImages$: BehaviorSubject<[]>;
-  constructor(private http: HttpClient) {
+public searchImages$: BehaviorSubject<any>;
+  constructor() {
     this.searchImages$ = new BehaviorSubject(null);
    }
 
