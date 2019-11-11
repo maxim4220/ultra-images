8e7145ed6da56854bf48f905529c4792acaa9750
Maxim Baranov <maxim4220@gmail.com>
2019-11-11 09:21:20 +0200

    Added imports and exports to UI module

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
 import { ImageSearchComponent } from './image-search/image-search.component';
-
-
+import { SpinnerComponent } from './spinner/spinner.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
-  declarations: [ImageSearchComponent],
+  declarations: [ImageSearchComponent, SpinnerComponent, PaginationComponent],
   imports: [
     CommonModule,
-
+  ],
+  exports: [
+    ImageSearchComponent, SpinnerComponent, PaginationComponent
   ]
 })
 
