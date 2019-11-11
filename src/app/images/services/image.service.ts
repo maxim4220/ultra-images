88b39af6cf700674e79081a1d4b888a7cd9511c5
Maxim Baranov <maxim4220@gmail.com>
2019-11-11 16:02:30 +0200

    Added comment

--- src/app/images/services/image.service.ts
+++ src/app/images/services/image.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ImageService {
-  private link = "http://api.giphy.com/v1/gifs/search?api_key=krYLN9xmR99j1WQkm5nERGA0w3bXmD2D&q=";
+  // Link includes api address and api application key.
+  private readonly link = "http://api.giphy.com/v1/gifs/search?api_key=krYLN9xmR99j1WQkm5nERGA0w3bXmD2D";
 
   constructor(private http: HttpClient,) { }
 
@@ -15,4 +16,5 @@ export class ImageService {
     const url = this.link + searchParams;
     return this.http.get(url);
 }
+
 }
