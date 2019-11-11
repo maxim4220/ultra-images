da69b97a3630807eb45defd0ff99608cfd5b9f8b
Maxim Baranov <maxim4220@gmail.com>
2019-11-11 16:00:11 +0200

    Added code for the pagination.

--- src/app/ui/pagination/pagination.component.ts
+++ src/app/ui/pagination/pagination.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.sass']
 })
-export class PaginationComponent implements OnInit {
 
-  constructor() { }
+export class PaginationComponent {
+  public lastPage: number;
+  public currentPage: number;
+  
+  @Input() pagination: any;
+  @Output() emitPagination = new EventEmitter<string>();
 
-  ngOnInit() {
+  public startPagination(offset) {
+   this.emitPagination.emit(offset);
   }
 
 }
