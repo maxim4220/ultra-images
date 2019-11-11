c9b04ec0568f97f6250c1145f77db94f16ec885d
Maxim Baranov <maxim4220@gmail.com>
2019-11-11 09:21:00 +0200

    Added UI module to imports

--- src/app/images/images.module.ts
+++ src/app/images/images.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagesComponent } from './images.component';
 import { Routes, RouterModule } from '@angular/router';
+import { UiModule } from '../ui/ui.module';
 
 const routes: Routes = [
   {
@@ -22,6 +23,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    UiModule
   ]
 })
 
