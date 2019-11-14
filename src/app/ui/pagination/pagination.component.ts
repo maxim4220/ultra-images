056b0f244437be4c95c0b583a8141598e8573aae
Maxim Baranov <maxim4220@gmail.com>
2019-11-14 09:54:32 +0200

    formatting

--- src/app/ui/pagination/pagination.component.ts
+++ src/app/ui/pagination/pagination.component.ts
@@ -1,33 +1,35 @@
-import { Component, EventEmitter, Input, Output, OnChanges } from '@angular/core'
-import { Pagination } from '../interfaces/pagination.interface'
+import { Component, EventEmitter, Input, Output, OnChanges } from '@angular/core';
+import { Pagination } from '../interfaces/pagination.interface';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.sass'],
+  styleUrls: ['./pagination.component.sass']
 })
-export class PaginationComponent implements OnChanges {
-  public currentPage: number
-  public countStep = 25
-  public showNextButton = true
 
-  @Input() pagination: Pagination
-  @Output() emitPagination = new EventEmitter<string>()
+export class PaginationComponent implements OnChanges {
+  public currentPage: number;
+  public countStep = 25;
+  public showNextButton = true;
+
+  @Input() pagination: Pagination;
+  @Output() emitPagination = new EventEmitter<string>();
 
   ngOnChanges(): void {
-    this.showNextButton = this.pagination.total_count !== this.pagination.offset + this.pagination.count
+    this.showNextButton = this.pagination.total_count !== this.pagination.offset + this.pagination.count;
     if (this.pagination.offset === 0) {
-      this.currentPage = 1
+      this.currentPage = 1;
     } else {
       if (this.pagination.offset % this.pagination.count === 0) {
-        this.currentPage = this.pagination.offset / this.pagination.count + 1
+        this.currentPage = this.pagination.offset / this.pagination.count + 1;
       } else {
-        this.pagination.count < this.countStep ? this.currentPage++ : this.currentPage--
+        this.pagination.count < this.countStep ? this.currentPage++ : this.currentPage--;
       }
     }
   }
 
   public startPagination(offset) {
-    return this.emitPagination.emit(offset)
+    return this.emitPagination.emit(offset);
   }
+
 }
