edbaf3a6829a1b712699808f691f302f008bc711
Maxim Baranov <maxim4220@gmail.com>
2019-11-14 09:53:36 +0200

    formatting

--- src/app/ui/image-search/image-search.component.ts
+++ src/app/ui/image-search/image-search.component.ts
@@ -1,18 +1,16 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Output, EventEmitter } from '@angular/core'
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-image-search',
   templateUrl: './image-search.component.html',
-  styleUrls: ['./image-search.component.sass']
+  styleUrls: ['./image-search.component.sass'],
 })
-
 export class ImageSearchComponent {
-  @Output() emitSearch = new EventEmitter<string>();
-  public search: FormControl = new FormControl('');
+  @Output() emitSearch = new EventEmitter<string>()
+  public search: FormControl = new FormControl('')
 
   public performSearch(): void {
-    this.emitSearch.emit(this.search.value);
+    this.emitSearch.emit(this.search.value)
   }
-
 }
